c22801a424cd1f7deadff52ba60e860f1b9a906e
karchaoui Abdessamad <140338304+abdok10@users.noreply.github.com>
2024-10-08 13:27:41 +0100

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="max-w-xl mx-auto ">
+      <h1>URL Shortener</h1>
+      <p>Shorten you URLs and share them easily</p>
+    </main>
+  );
+}
